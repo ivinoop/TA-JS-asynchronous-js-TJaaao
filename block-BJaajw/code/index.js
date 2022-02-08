04fdd58cdda04dc312e483b570ed4cab119ebbae
Vinoo <ivinoop@gmail.com>
2022-02-08 18:31:29 +0530

    re

--- block-BJaajw/code/index.js
+++ block-BJaajw/code/index.js
@@ -2,8 +2,16 @@ let url = 'https://api.spaceflightnewsapi.net/v3/articles?_limit=30';
 
 let newsList = document.querySelector('.news-list');
 let select = document.querySelector('select');
+let main = document.querySelector('.main');
+let errorMessage = document.querySelector('.error-message');
 
 let allNews = [];
+
+function handleErrorMessage(message = 'Something went wrong') {
+  main.style.display = 'none';
+  errorMessage.style.display = 'block';
+  errorMessage.innerText = message;
+}
 
 function displayLoader() {
   let div = document.createElement('div');
@@ -52,14 +60,29 @@ function displayOptions(sources) {
   })
 }
 
-fetch(url)
-.then((resolve) => resolve.json())
-.then((news) => {
-  allNews = news;
-  renderNews(news);
-  let allSources = Array.from(new Set(news.map(n => n.newsSite)));
-  displayOptions(allSources);
-});
+function init() {
+  fetch(url)
+  .then((response) => {
+    if(response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Response Not Ok');
+    }
+  })
+  .then((news) => {
+    allNews = news;
+    renderNews(news);
+    let allSources = Array.from(new Set(news.map(n => n.newsSite)));
+    displayOptions(allSources);
+  })
+  .catch(error => {
+    handleErrorMessage(error);
+  })
+  .finally(() => {
+    displayLoader();
+  });
+}
+
 
 select.addEventListener('change', (event) => {
   let source = event.target.value.trim();
@@ -71,3 +94,9 @@ select.addEventListener('change', (event) => {
   }
   renderNews(filteredNews);
 });
+
+if(navigator.onLine) {
+  init();
+} else {
+  handleErrorMessage('Check your internet connection ⚠️');
+}
